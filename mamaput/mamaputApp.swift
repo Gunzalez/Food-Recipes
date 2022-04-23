db62e8fdc8dd0d93c822723c6c547e1e188716d7
Segun Konibire <segun.konibire@easeltv.com>
2022-04-23 12:59:27 +0100

    Initial Commit

---
+++ mamaput/mamaputApp.swift
@@ -0,0 +1,17 @@
+//
+//  mamaputApp.swift
+//  mamaput
+//
+//  Created by Ségun Konibire on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct mamaputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
