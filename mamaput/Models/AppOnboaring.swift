c43ca7ec832d39ecd55bdbf223894d10be5e72be
Segun Konibire <segun.konibire@easeltv.com>
2022-04-24 18:04:17 +0100

    Adds in Lottie resources

--- mamaput/Models/AppOnboaring.swift
+++ mamaput/Models/AppOnboaring.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-struct AppOnboaring: Identifiable {
+struct AppOnboarding: Identifiable {
     let id = UUID().uuidString
     var title: String
     var detail: String
     var animationName: String
 }
 
-extension AppOnboaring {
-    static var data: [AppOnboaring] {
+extension AppOnboarding {
+    static var data: [AppOnboarding] {
         [
-            AppOnboaring(title: "Welcome", detail: "Pick a meal, cook a meal", animationName: "animation1"),
-            AppOnboaring(title: "Welcome", detail: "Pick a meal, cook a meal", animationName: "animation2"),
-            AppOnboaring(title: "Welcome", detail: "Pick a meal, cook a meal", animationName: "animation3"),
-            AppOnboaring(title: "Welcome", detail: "Pick a meal, cook a meal", animationName: "animation4"),
+            AppOnboarding(title: "Welcome to Food Recipes!", detail: "Search, cook and enjoy freshly made food at your home. Find step by step recipes with Food Recipes App.", animationName: "onboarding1"),
+            AppOnboarding(title: "Pick fresh ingredients!", detail: "Best tasting food comes from fresh ingredients. Find list of ingredients with Food Recipes App.", animationName: "onboarding2"),
+            AppOnboarding(title: "Cook to perfection!", detail: "Delicious food takes its own time to get ready. Find list of steps to cook your food to the perfection.", animationName: "onboarding3"),
+            AppOnboarding(title: "Enjoy your homemade meal!", detail: "Enjoy your home cooked food with your friends and family.", animationName: "onboarding4"),
         ]
     }
 }
---
+++ mamaput/Utils/LottieView.swift
@@ -0,0 +1,40 @@
+//
+//  LottieView.swift
+//  mamaput
+//
+//  Created by Ségun Konibire on 24/04/2022.
+//
+
+import Lottie
+import SwiftUI
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    
+    var name = ""
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        let animationView = AnimationView()
+        let animation = Animation.named(name)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(animationView);
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {}
+    
+}
