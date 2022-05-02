efef7a3b81bdb9dd1a10754dc468a7ac2b097c1e
Segun Konibire <segun.konibire@easeltv.com>
2022-05-03 00:54:38 +0100

    creates general purpose JSON parser

--- mamaput/Views/Home/RecipesListView.swift
+++ mamaput/Views/Home/RecipesListView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct RecipesListView: View {
+    
+    @State private var allreceipies = [Recipe]()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear {
+                allreceipies = JSONFileManager.load("recipes.json")
+                print(allreceipies)
+            }
     }
 }
 
