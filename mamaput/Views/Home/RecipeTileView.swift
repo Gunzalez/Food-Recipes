109dfb0970e29a91c39cd9fad54f9b18b56a357a
Segun Konibire <segun.konibire@easeltv.com>
2022-05-11 08:42:29 +0100

    creates list view for recipes

--- mamaput/Views/Home/RecipeTileView.swift
+++ mamaput/Views/Home/RecipeTileView.swift
@@ -17,23 +17,24 @@ struct RecipeTileView: View {
             .resizable()
             .scaledToFill()
             .overlay(LinearGradient(colors: [.black.opacity(0.5), .clear, .black.opacity(0.5)], startPoint: .top, endPoint: .bottomTrailing))
-            .frame(width: UIScreen.main.bounds.width * 0.9, height: 280)
-            .clipped()
+            .frame(width: UIScreen.main.bounds.width * 0.9, height: 300)
+//            .clipped()
+            .clipShape(RoundedRectangle(cornerRadius: 20))
     }
     
     @ViewBuilder
     private var recipeLabel: some View {
         HStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 20){
-                Text(recipe.name).font(.title)
+                Text(recipe.name).font(.title).frame(alignment: .leading)
                 Text("\(recipe.cookTime) | \(recipe.servings)")
                     .foregroundColor(.secondary)
             }
-            .frame(width: UIScreen.main.bounds.width * 0.8, alignment: .leading)
+            .frame(maxWidth: UIScreen.main.bounds.width * 0.8, alignment: .leading)
         }
         .padding()
         .background(.thinMaterial, in:
-            RoundedRectangle(cornerRadius: 20))
+            RoundedRectangle(cornerRadius: 16))
     }
     
     var body: some View {
@@ -48,7 +49,9 @@ struct RecipeTileView: View {
                 Spacer()
                 recipeLabel
             }
+            .padding()
         }
+//        .clipShape(RoundedRectangle(cornerRadius: 20))
     }
 }
 
--- mamaput/Views/Home/RecipesListView.swift
+++ mamaput/Views/Home/RecipesListView.swift
@@ -9,14 +9,41 @@ import SwiftUI
 
 struct RecipesListView: View {
     
+    @State private var searchText = ""
     @State private var allreceipies = [Recipe]()
     
+    var recipes: [Recipe] {
+        if(searchText.isEmpty){
+            return allreceipies
+        }
+        
+        return allreceipies.filter({ $0.name.lowercased().contains(searchText.lowercased()) })
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear {
-                allreceipies = JSONFileManager.load("recipes.json")
-                print(allreceipies)
+        NavigationView {
+            VStack {
+                SearchView(searchText: $searchText)
+                ScrollView(showsIndicators: false) {
+                    LazyVStack(alignment: .leading, spacing: 10){
+                        BannerView()
+                        ForEach(recipes, id: \.self) { recipe in
+                            NavigationLink(destination: Text(recipe.name)){
+                                RecipeTileView(recipe: recipe)
+                            }
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
             }
+            .animation(.easeIn, value: searchText)
+            .padding(.horizontal)
+            .navigationTitle("Mama Put")
+            
+        }
+        .onAppear {
+            allreceipies = JSONFileManager.load("recipes.json")
+        }
     }
 }
 
